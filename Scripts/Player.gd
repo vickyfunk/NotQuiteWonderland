dbff27afe9d46b005649bbe74af2b73fc12680c9
bunnygirlpilled <106631931+bunnygirlpilled@users.noreply.github.com>
2023-08-17 18:37:52 -0500

    added fps controller

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -49,7 +49,7 @@ func _physics_process(delta):
 		speed = WALK_SPEED
 
 	# Get the input direction and handle the movement/deceleration.
-	var input_dir = Input.get_vector("left", "right", "forward", "backward")
+	var input_dir = Input.get_vector("left", "right", "up", "down")
 	var direction = (head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if is_on_floor():
 		if direction:
