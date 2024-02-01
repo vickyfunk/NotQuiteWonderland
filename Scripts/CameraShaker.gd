7b3931745714bd2f71bc06ca7f4ddbe71f945ade
vickyfunk <33412798+vickyfunk@users.noreply.github.com>
2024-01-31 19:14:40 -0600

    recoil tweaks
    looks better, works better, tantal tuned

--- Scripts/CameraShaker.gd
+++ Scripts/CameraShaker.gd
@@ -4,16 +4,18 @@ class_name CameraShaker
 
 #Trauma will decrease by this amount every second
 #(Note, trauma is clamped from 0.0 to 1.0
-@export var trauma_reduction_rate = 1.0
+@export var trauma_reduction_rate = .5
 var trauma = 0.0
+var potential_trauma = 0.0
 
 @export var noise: FastNoiseLite
 @export var noise_speed = 50.0
 var time = 0.0
 
-@export var max_x = 10.0
-@export var max_y = 10.0
-@export var max_z = 5.0
+@export var max_x = 4.0
+@export var max_y = 6.0
+@export var max_z = 3.0
+@export var lerp_speed = 1.0
 
 @export var camera: Camera3D
 @onready var initial_rotation = camera.rotation_degrees as Vector3
@@ -22,9 +24,9 @@ func _physics_process(delta):
 	time += delta
 	trauma = max(trauma - delta * trauma_reduction_rate, 0.0)
 	
-	camera.rotation_degrees.x = initial_rotation.x + max_x * get_shake_intensity() * get_noise_from_seed(0)
-	camera.rotation_degrees.y = initial_rotation.y + max_y * get_shake_intensity() * get_noise_from_seed(1)
-	camera.rotation_degrees.z = initial_rotation.z + max_z * get_shake_intensity() * get_noise_from_seed(2)
+	camera.rotation_degrees.x = lerp(camera.rotation_degrees.x, initial_rotation.x + max_x * get_shake_intensity() * get_noise_from_seed(0), lerp_speed * delta)
+	camera.rotation_degrees.y = lerp(camera.rotation_degrees.y, initial_rotation.y + max_y * get_shake_intensity() * get_noise_from_seed(1), lerp_speed * delta)
+	camera.rotation_degrees.z = lerp(camera.rotation_degrees.z, initial_rotation.z + max_z * get_shake_intensity() * get_noise_from_seed(2), lerp_speed * delta)
 
 func add_trauma(trauma_amount: float):
 	trauma = clamp(trauma + trauma_amount, 0.0, 1.0)
